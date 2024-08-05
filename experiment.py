013cfede3812a07aeb0649831823ec3ee7c08165
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2024-08-04 23:01:45 -0400

    Updated the environment script and requirements

--- experiment.py
+++ experiment.py
@@ -1,8 +1,20 @@
+import click
 
 
+@click.command()
 
-def main():
-    pass
+@click.option("--avatar")
+@click.option("--command")
+
+def main(avatar, command, **config_kwargs):
+    """
+    Lets test this
+    :param avatar:
+    :param command:
+    :param config_kwargs:
+    :return:
+    """
+
 
 
 if __name__ == '__main__':
